5f2d868045cd80c18f4ea6eee9f9ca19212270dd
Shush1k <56314194+Shush1k@users.noreply.github.com>
2020-03-11 19:51:30 +0300

    какая-то дичь

---
+++ Практика Алгоритмы Python3/Semestr2_algorithm/pract2/Task1_test/classFigure.py
@@ -0,0 +1,13 @@
+import methoods
+class Figure:
+    """С методами вычисления площади и периметра"""
+    def __init__(self, a, b, c, r, ):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.r = r
+
+    get_square = methoods.get_square
+    get_perimeter = methoods.get_perimeter
+    info = methoods.info
+
