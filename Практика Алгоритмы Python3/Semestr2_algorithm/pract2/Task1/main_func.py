93dd02af7b2c06bcb139113d3c04bef67aaca96e
Shush1k <56314194+Shush1k@users.noreply.github.com>
2020-03-12 10:49:05 +0300

    до 25 строк

--- Практика Алгоритмы Python3/Semestr2_algorithm/pract2/Task1/main_func.py
+++ Практика Алгоритмы Python3/Semestr2_algorithm/pract2/Task1/main_func.py
@@ -9,20 +9,10 @@ def main():
     except ValueError:
         print("0_0 упс...")
         return
-
-    d = {
-        1: classRectangle.Rectangle,
-        2: classCircle.Circle,
-        3: classTriangle.Triangle
-    }
+    d = {1: classRectangle.Rectangle, 2: classCircle.Circle, 3: classTriangle.Triangle}
     figures_list = []
     for _ in range(n):
-
-        d_val = {
-            1: [randint(1, 100), randint(1, 100)],
-            2: [randint(1, 100)],
-            3: [randint(1, 100), randint(1, 100), randint(1, 100)]
-        }
+        d_val = {1: [randint(1, 100), randint(1, 100)], 2: [randint(1, 100)], 3: [randint(1, 100), randint(1, 100), randint(1, 100)]}
 
         r = randint(1, 3)
         figures_list.append(d[r](*d_val[r]))
