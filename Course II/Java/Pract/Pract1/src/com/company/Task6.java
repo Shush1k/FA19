09a61129bcacd514239e3129d58bc193c6db6da6
Shush1k <shush1k.b@gmail.com>
2020-09-09 23:05:08 +0300

    вернемся

--- Course II/Java/Pract/Pract1/src/com/company/Task6.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Task6 {
-    //Дано натуральное число. Выведите его последнюю цифру.
-    public static void main(String[] args) {
-        int abc = 123;
-        System.out.println(abc%10);
-    }
-}
