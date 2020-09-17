a90491f19bb3c43ecf24a0d88036a33f87d3f972
Shush1k <shush1k.b@gmail.com>
2020-09-17 11:25:05 +0300

    часть второй задачи(not ready)

--- Course II/Java/Pract/Pract2/Task2/src/com/shush1k/Main.java
+++ Course II/Java/Pract/Pract2/Task2/src/com/shush1k/Main.java
@@ -3,6 +3,18 @@ package com.shush1k;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("32");
+        Vector vect1 = new Vector(3, 4, 5);
+        Vector vect2 = new Vector(3, 6, 10);
+        System.out.println("Вектор 1:");
+	    vect1.getValue();
+        System.out.println("Вектор 2:");
+        vect2.getValue();
+	    System.out.println("\nДлина вектора 1:\n"+vect1.Len());
+	    System.out.println("Скалярное произведение:\n"+vect1.Scalar(vect2));
+        System.out.println("Векторное произведение:\n");
+        vect1.Multi(vect2).getValue();
+        System.out.println("Косинус угла:\n"+vect1.VectorAngle(vect2));
+
     }
+
 }
---
+++ Course II/Java/Pract/Pract2/Task2/src/com/shush1k/Vector.java
@@ -0,0 +1,47 @@
+package com.shush1k;
+/*Создайте класс, который описывает вектор (в трёхмерном пространстве).
+У него должны быть:
+a. конструктор с параметрами в виде списка координат x, y, z
+b. метод, вычисляющий длину вектора. Корень можно посчитать с помощью
+Math.sqrt()
+c. метод, вычисляющий скалярное произведение
+d. метод, вычисляющий векторное произведение с другим вектором
+e. метод, вычисляющий угол между векторами (или косинус угла): косинус угла
+между векторами равен скалярному произведению векторов, деленному на
+произведение модулей (длин) векторов
+f. методы для суммы и разности
+g. статический метод, который принимает целое число N, и возвращает массив
+случайных векторов размером N
+h. Если метод возвращает вектор, то он должен возвращать новый объект, а не
+менять базовый*/
+public class Vector {
+    int x;
+    int y;
+    int z;
+    public Vector(int x, int y, int z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    public void getValue(){
+        // Вывод вектора
+        System.out.format("(%d, %d, %d)\n", x, y, z);
+    }
+    public double Len(){
+        // Длина вектора
+        return Math.sqrt(Math.pow(x, 2)+ Math.pow(y, 2)+ Math.pow(z, 2));
+    }
+    public double Scalar(Vector vector){
+        // Скалярное пр-ие
+        return x*vector.x + y*vector.y + z*vector.z;
+    }
+    public Vector Multi(Vector vector){
+        // Векторное пр-ие
+        return new Vector(y*vector.z - z*vector.y,z*vector.x - x*vector.z, x*vector.y - y*vector.x);
+    }
+    public double VectorAngle(Vector vector){
+        // Угол между векторами
+        return Scalar(vector)/ (Math.abs(vector.Len())*Math.abs(Len()));
+    //Сумма и разность и  Static
+    }
+}
