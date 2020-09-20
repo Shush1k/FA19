618c9070a6b3e3f339fd5980041d130fff5dc548
Shush1k <shush1k.b@gmail.com>
2020-09-20 16:44:54 +0300

    не понимаю задачу

--- Course II/Java/Pract/Pract2/Task5/src/com/shush1k/Main.java
+++ Course II/Java/Pract/Pract2/Task5/src/com/shush1k/Main.java
@@ -3,6 +3,10 @@ package com.shush1k;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+    int[] values = {1, 2, 3};
+    int[] scales = {1, 2, 10};
+    Arrays arrays = new Arrays(values, scales);
+    //int getValue =
+    //System.out.println("Полученное значение: "+getValue);
     }
 }
