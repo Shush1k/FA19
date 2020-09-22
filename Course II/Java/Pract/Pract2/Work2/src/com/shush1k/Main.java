13e2c254b905ffea00c1ecc61073ad262182ee51
Shush1k <shush1k.b@gmail.com>
2020-09-22 11:35:24 +0300

    Work2

--- Course II/Java/Pract/Pract2/Work2/src/com/shush1k/Main.java
+++ Course II/Java/Pract/Pract2/Work2/src/com/shush1k/Main.java
@@ -1,13 +1,16 @@
 package com.shush1k;
 import java.util.*;
-
+/*
+1)Реализовать метод, который принимает коллекцию и возвращает коллекцию без вопросов
+2) Постройте частотный словарь букв алфавита. На вход метод принимает строку.
+*/
 public class Main {
 
     public static void main(String[] args) {
         //Первая задача
         System.out.println("Первая задача");
         //Вызов через статический метод
-        Main.buildDict("аывоыоаоыфарыфаоыфдвдцц");
+        Main.buildDict("аывоыоаоыфарыфаоыфдвдцциклмно");
         //Вторая задача
         System.out.println("Вторая задача");
         ArrayList<Integer> intList = new ArrayList<>();
@@ -43,7 +46,46 @@ public class Main {
         }
     }
 
+    public static void buildDictWithMap(String text){
+        text = text.toLowerCase();
+        //словарь map
+        Map<Character, Integer> map = new HashMap<>();
 
+        for(int i=0; i<text.length(); i++){
+            char ch = text.charAt(i);
+            // Проверка на русские буквы
+            if((ch>= 'а' && ch <= 'я' ) || ch == 'ё'){
+                // передаем ch(символ) получаем пару ключ значение; compute - присвоение значения. lambda
+                map.compute(ch, ((character, integer) -> integer == null ? 1: integer +1));
+            }
+        }
+        //Набор элементов коллекции(Map.Entry) .entrySet - возвращает множество пар ключ значение
+        ArrayList<Map.Entry<Character, Integer>> entries = new ArrayList<>(map.entrySet());
+        // o1, o2 - объекты. compare сравнивание двух символов(ключей объектов)
+        entries.sort(((o1, o2) -> Character.compare(o1.getKey(), o2.getKey())));
+        for (Map.Entry<Character, Integer>enter : entries){
+            System.out.println(enter.getKey()+" = "+ enter.getValue());
+        }
+    }
+    public static <K, V> Map<V, Collection<K>> revertMap(Map<? extends K, ? extends V> map) {
+        Map<V, Collection<K>> resultMap = new HashMap<>();
+
+        Set<K> keys = (Set<K>) map.keySet();
+        for(K key:keys){
+            //получаем значение из словаря map по ключу
+            V value = map.get(key);
+            //функция
+            resultMap.compute(value, (v, ks) ->{
+                if (ks == null){
+                    //создаем множество, если ничего нет
+                    ks = new HashSet<>();
+                }
+                ks.add(key);
+                return ks;
+            });
+        }
+        return resultMap;
+    }
     public static <T>Collection<T> removeDuplications(Collection<T> coll){
         /** Метод ко второй задаче*/
         return new HashSet<>(coll);
