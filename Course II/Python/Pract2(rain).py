51aa02ccb4ee96a5868904bbef8dc96a07591082
Shush1k <shush1k.b@gmail.com>
2020-10-12 22:58:57 +0300

    Pract2 - Rain(Done)
    Добавлено:
    1)col_range - нужен для других размерностей списка цветов
    2)min_len/max_len - для наглядности
    Изменено:
    Выбрал цвета поприятнее

--- Course II/Python/Pract2(rain).py
+++ Course II/Python/Pract2(rain).py
@@ -37,23 +37,27 @@ canvas = tk.Canvas(root, width=width, height=height)
 canvas.pack()
 drops = []
 # Основной список цветов
-colors = [*["#6557E9"]*3, *["#0000cc"]*3, *["#000099"]*3, *["#6996D3"]*3]
-# colors = [*["#6557E9"]*3, *["#4380D3"]*3, *["#3f92d2"]*3, *["#2C17B1"]*3]
-
+colors = [*["blue"]*3, *["blue2"]*3, *["#77a1b5"]*3, *["#6996D3"]*3]
+# colors = [*["#6557E9"]*3, *["#0000cc"]*3, *["#000099"]*3, *["#6996D3"]*3]
 # Градиент
 # colors = ["blue2","blue", "dodger blue", "RoyalBlue2", "RoyalBlue1", "cornflower blue", "DeepSkyBlue2", "SteelBlue2", "SteelBlue1", "SkyBlue2","SkyBlue1"]
-
+print(list(reversed(colors)))
+print(list(reversed(colors))[int(11)])
+# default: step = 1.5; min_len = 4; max_len = 15;
+step = 1.5 # коэффициент изменение длины полосок
+min_len = 4
+max_len = 15
+col_range = max_len - len(colors) + 1 # нужен для реализации шага
+# print(col_range)
 for i in range(100):
-    step = 1.5
-    length = randint(4, 15)*step
-    width_line = 0.1 * length
-    # print(length, list(reversed(colors))[int(length//step)-7])
+    length = randint(min_len, max_len)*step
+    width_line = 0.09 * length
+    # print(length, list(reversed(colors))[int(length//step)-col_range])
     if length > 14:
-        drops.append(Raindrop(canvas, width, height, x=randint(0, width), y=randint(0, height), yspeed=randint(5, 9), length=length, color=list(reversed(colors))[int(length//step)-7], width_line=width_line))
+        drops.append(Raindrop(canvas, width, height, x=randint(0, width), y=randint(0, height), yspeed=randint(5, 9), length=length, color=list(reversed(colors))[int(length//step)-col_range], width_line=width_line))
 
     if length <15:
-        # print(length, list(reversed(colors))[int(length//step)-7])
-        drops.append(Raindrop(canvas, width, height, x=randint(0, width), y=randint(0, height), yspeed=randint(2, 4), length=length,color=list(reversed(colors))[int(length//step)-7], width_line=width_line))
+        drops.append(Raindrop(canvas, width, height, x=randint(0, width), y=randint(0, height), yspeed=randint(2, 4), length=length,color=list(reversed(colors))[int(length//step)-col_range], width_line=width_line))
 
 
 redraw()
