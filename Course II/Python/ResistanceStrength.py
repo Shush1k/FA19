2289aca521dc6428b447daadae3eff61be734a0b
Shush1k <shush1k.b@gmail.com>
2020-10-22 11:53:51 +0300

    Update ResistanceStrength.py

--- Course II/Python/ResistanceStrength.py
+++ Course II/Python/ResistanceStrength.py
@@ -22,22 +22,51 @@ class Ball:
         self.y2 = y2
         self.radius = radius
         self.c = c
-        # coeff_radius можно придумать что-нибудь интереснее
-        self.coeff_radius = radius/5
+        # resistance можно придумать что-нибудь интереснее
+        self.resistance = radius*0.2
         self.ball = c.create_oval(self.x1, self.y1, self.x2, self.y2, fill=color, outline='white', width=2)
 
     def move_ball(self):
         # Отпринтовка координат шара
         # print(c.coords(self.ball)[3])
+        
         if c.coords(self.ball)[3] < 300:
             c.move(self.ball, 0, 9)
             c.after(1000//60, self.move_ball)
         elif 300 <= c.coords(self.ball)[3] < 600:
-            c.move(self.ball, 0, self.coeff_radius)
+            c.move(self.ball, 0, self.resistance)
             c.after(1000//60, self.move_ball)
+        elif c.coords(self.ball)[3] >= 600:
+            c.after(1000//60, self.iter_1)
 
-        #TODO если координата 600, сделать отскакивание
+        #TODO создать подъем частицы на высоту, зависящую от массы!!!
         #Добавьте учёт инерции частиц в зависимости от их массы.
+    def iter_1(self):
+        if c.coords(self.ball)[3] >= 585:
+            c.move(self.ball, 0, -self.resistance)
+            c.after(1000//60, self.iter_1)
+        elif c.coords(self.ball)[3] <= 600:
+            c.after(1000//60, self.iter_2)
+    def iter_2(self):
+        if c.coords(self.ball)[3] < 600:
+            c.move(self.ball, 0, 0.4*self.resistance)
+            c.after(1000//60, self.iter_2)
+        else:
+            c.after(1000//60, self.iter_3)
+
+    def iter_3(self):
+        if c.coords(self.ball)[3] >= 595:
+            c.move(self.ball, 0, 0.2*-self.resistance)
+            c.after(1000//60, self.iter_3)
+        elif c.coords(self.ball)[3] <= 600:
+            c.after(1000//60, self.iter_stop)
+
+    def iter_stop(self):
+        if c.coords(self.ball)[3] <= 600:
+            c.move(self.ball, 0, 0.5)
+            c.after(1000//60, self.iter_stop)
+        
+
 
 def esc(event):
     root.destroy()
@@ -61,6 +90,10 @@ ball2.move_ball()
 
 r4 = 35
 ball2 = Ball(c, 400 - r4, 120 - r4, 400 + r4, 120 + r4, r4, color="#a0a0a0")
+ball2.move_ball()
+
+r4 = 15
+ball2 = Ball(c, 530 - r4, 120 - r4, 530 + r4, 120 + r4, r4, color="#a0a0a0")
 ball2.move_ball()
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
