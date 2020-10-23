a3606aefae0113d7bf1fdc8328589cf17e2ec3fe
Shush1k <shush1k.b@gmail.com>
2020-10-23 22:39:40 +0300

    mini fix

--- Course II/Java/Pract/ClassWork/src/com/tictactoe/tictactoe/TicTacToe.java
+++ Course II/Java/Pract/ClassWork/src/com/tictactoe/tictactoe/TicTacToe.java
@@ -80,34 +80,16 @@ public class TicTacToe {
             symbol = 'O';
             compPositions.add(pos);
         }
-        switch (pos){
-            case 1:
-                gameBoard[0][0] = symbol;
-                break;
-            case 2:
-                gameBoard[0][2] = symbol;
-                break;
-            case 3:
-                gameBoard[0][4] = symbol;
-                break;
-            case 4:
-                gameBoard[2][0] = symbol;
-                break;
-            case 5:
-                gameBoard[2][2] = symbol;
-                break;
-            case 6:
-                gameBoard[2][4] = symbol;
-                break;
-            case 7:
-                gameBoard[4][0] = symbol;
-                break;
-            case 8:
-                gameBoard[4][2] = symbol;
-                break;
-            case 9:
-                gameBoard[4][4] = symbol;
-                break;
+        switch (pos) {
+            case 1 -> gameBoard[0][0] = symbol;
+            case 2 -> gameBoard[0][2] = symbol;
+            case 3 -> gameBoard[0][4] = symbol;
+            case 4 -> gameBoard[2][0] = symbol;
+            case 5 -> gameBoard[2][2] = symbol;
+            case 6 -> gameBoard[2][4] = symbol;
+            case 7 -> gameBoard[4][0] = symbol;
+            case 8 -> gameBoard[4][2] = symbol;
+            case 9 -> gameBoard[4][4] = symbol;
         }
     }
     public static String checkWinner(){
