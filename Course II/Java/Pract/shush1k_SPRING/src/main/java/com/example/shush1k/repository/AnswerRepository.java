3b93f80493b5372517c10c75da4bee338bde2413
Shush1k <shush1k.b@gmail.com>
2020-12-01 11:49:39 +0300

    Spring

---
+++ Course II/Java/Pract/shush1k_SPRING/src/main/java/com/example/shush1k/repository/AnswerRepository.java
@@ -0,0 +1,10 @@
+package com.example.shush1k.repository;
+
+import com.example.shush1k.model.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+    List<Answer> findByQuestionId(Long questionId);
+}
