8322a413f63ac961e78ea9ceffcf84fe42bc71ce
Shush1k <shush1k.b@gmail.com>
2021-02-10 15:38:42 +0300

    Revert "java Spring"
    This reverts commit c97a35175ee4e903ae703d92f47d7fb21d230bf8.

--- Course II/Java/Pract/shush1k_SPRING/src/main/java/com/example/shush1k/repository/QuestionRepository 2.java
+++
@@ -1,7 +0,0 @@
-package com.example.shush1k.repository;
-
-import com.example.shush1k.model.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionRepository extends JpaRepository<Question, Long> {
-}
