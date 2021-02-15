b09c290faa036c87032201b4c6b27a57e022ab23
Shush1k <shush1k.b@gmail.com>
2021-02-15 17:21:30 +0300

    #14 add all from Ggl drive

---
+++ Course II/Java/Pract/javafx_pi19-3/src/program/Main.java
@@ -0,0 +1,84 @@
+package program;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import program.controllers.PersonOverviewController;
+import program.models.Person;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+    private ObservableList<Person> personData = FXCollections.observableArrayList();
+
+    public Main(){
+        for(int i=0; i<40; i++){
+            personData.add(new Person("Имя" + i, "Фамилия" + i));
+        }
+    }
+
+    public ObservableList<Person> getPersonData() {
+        return personData;
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Address Application");
+
+        initRootLayout();
+
+        showPersonOverview();
+    }
+
+    public void initRootLayout(){
+        try{
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("views/root.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public void showPersonOverview(){
+        try{
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("views/main.fxml"));
+            AnchorPane personOverview = (AnchorPane) loader.load();
+
+            rootLayout.setCenter(personOverview);
+
+            PersonOverviewController controller = loader.getController();
+            controller.setMain(this);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public BorderPane getRootLayout() {
+        return rootLayout;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
