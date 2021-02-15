53f6e60fc4503af8e17b7a0c239b38e5ef1e6cdf
Shush1k <56314194+Shush1k@users.noreply.github.com>
2021-02-15 20:55:39 +0300

    попытка номер 2 one commit merge (#16)
    * #12 training scheme
    
    * #12 add pract 7
    
    * #12 add pract 14-15
    
    pract3-1 - university db
    
    * #12 add pract 16-18
    
    * #12 add pract 19-21
    
    * Create lab3.docx
    
    * upd lab3
    
    * #10 test
    
    * #10 rm test
    
    * #12 add pract1
    
    * add pract1/pract2 Python_SQL
    
    * Create T02_Core.ipynb
    
    * add DB example and pract2
    
    * add pract2 and pract3 SQL
    
    * #10 add lab3
    
    Показ работы с git системой и удаленным репозиторием на GitHub
    
    * #12 add pract2 and pract3
    
    * Create distance_fulltime_essay.docx
    
    * #14 add all from Ggl drive

---
+++ Course II/Java/Pract/javafx_pi19-3/src/program/models/Person.java
@@ -0,0 +1,98 @@
+package program.models;
+
+import javafx.beans.property.*;
+
+import java.time.LocalDate;
+
+public class Person {
+    private StringProperty firstName;
+    private StringProperty lastName;
+    private StringProperty street;
+    private StringProperty city;
+    private IntegerProperty postalCode;
+    private ObjectProperty<LocalDate> birthDay;
+
+
+    public Person(String firstName, String lastName){
+        this.firstName = new SimpleStringProperty(firstName);
+        this.lastName = new SimpleStringProperty(lastName);
+
+        // Какие-то фиктивные значения
+        this.street = new SimpleStringProperty("Какая-то улица");
+        this.city = new SimpleStringProperty("Какой-то город");
+        this.postalCode = new SimpleIntegerProperty(1234);
+        this.birthDay = new SimpleObjectProperty<LocalDate>(LocalDate.of(1995,9,12));
+    }
+
+    public String getFirstName() {
+        return firstName.get();
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName.set(firstName);
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName.set(lastName);
+    }
+
+    public void setStreet(String street) {
+        this.street.set(street);
+    }
+
+    public void setCity(String city) {
+        this.city.set(city);
+    }
+
+    public void setBirthDay(LocalDate birthDay) {
+        this.birthDay.set(birthDay);
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode.set(postalCode);
+    }
+
+    public String getLastName() {
+        return lastName.get();
+    }
+
+    public String getStreet() {
+        return street.get();
+    }
+
+    public String getCity() {
+        return city.get();
+    }
+
+    public int getPostalCode() {
+        return postalCode.get();
+    }
+
+    public LocalDate getBirthDay() {
+        return birthDay.get();
+    }
+
+    public StringProperty getFirstNameProperty() {
+        return firstName;
+    }
+
+    public StringProperty getLastNameProperty() {
+        return lastName;
+    }
+
+    public StringProperty getStreetProperty() {
+        return street;
+    }
+
+    public StringProperty getCityProperty() {
+        return city;
+    }
+
+    public IntegerProperty getPostalCodeProperty() {
+        return postalCode;
+    }
+
+    public ObjectProperty<LocalDate> getBirthDayProperty() {
+        return birthDay;
+    }
+}
