db4b76054d9ecc4caa69d6f3e4bb4d806ec1302f
Shush1k <shush1k.b@gmail.com>
2021-02-22 20:46:35 +0300

    java javaFX

---
+++ Course II/Java/Pract/JavaFX/src/program/controllers/PersonOverviewController.java
@@ -0,0 +1,48 @@
+package program.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import program.Main;
+import program.models.Person;
+
+public class PersonOverviewController {
+    @FXML
+    private TableView<Person> personTable;
+    @FXML
+    private TableColumn<Person, String> firstNameColumn;
+    @FXML
+    private TableColumn<Person, String> lastNameColumn;
+    @FXML
+    private Label firstNameLabel;
+    @FXML
+    private Label lastNameLabel;
+    @FXML
+    private Label streetLabel;
+    @FXML
+    private Label cityLabel;
+    @FXML
+    private Label postalCodeLabel;
+    @FXML
+    private Label birthdayLabel;
+
+    private Main main;
+
+    public PersonOverviewController(){}
+
+    @FXML
+    private void initialize(){
+        firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().getFirstNameProperty());
+        lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().getLastNameProperty());
+
+    }
+
+    public void setMain(Main main) {
+        this.main = main;
+
+        personTable.setItems(main.getPersonData());
+    }
+
+
+}
