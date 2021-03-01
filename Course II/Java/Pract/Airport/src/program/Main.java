7c0c10a3cfe5bf5d5b15892d8dd412d6a6c46088
Shush1k <shush1k.b@gmail.com>
2021-03-01 22:42:33 +0300

    #17 #14 main, person

--- Course II/Java/Pract/Airport/src/program/Main.java
+++ Course II/Java/Pract/Airport/src/program/Main.java
@@ -3,22 +3,28 @@ package program;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
-import program.controllers.flightController;
+import program.controllers.*;
+import program.models.Person;
 
 import java.io.IOException;
 
 public class Main extends Application {
     private Stage primaryStage;
-    private AnchorPane rootLayout;
+    private BorderPane rootLayout;
+    private ObservableList<Person> personData = FXCollections.observableArrayList();
+
+    public Main(){}
+
+    public ObservableList<Person> getPersonData() {
+        return personData;
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -26,32 +32,34 @@ public class Main extends Application {
         this.primaryStage.setTitle("Airport");
 
         initRootLayout();
-//        showSchedule();
+        showAuthorizationPage();
     }
+    @FXML
     public void initRootLayout() {
         try {
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(Main.class.getResource("views/aboutPage.fxml"));
-            rootLayout = (AnchorPane) loader.load();
+            loader.setLocation(Main.class.getResource("views/rootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
 
             Scene scene = new Scene(rootLayout);
             primaryStage.setScene(scene);
+//            primaryStage.setResizable(false);
+            RootLayoutController rootLayoutController = loader.getController();
+            rootLayoutController.setMain(this);
             primaryStage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void showSchedule(){
+    public void showAuthorizationPage(){
         try{
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(Main.class.getResource("views/airlineInfo.fxml"));
-            BorderPane personOverview = (BorderPane) loader.load();
-
-//            rootLayout.setCenter(personOverview);
-
-            flightController controller = loader.getController();
-//            controller.setMain(this);
+            loader.setLocation(Main.class.getResource("views/authorization.fxml"));
+            AnchorPane authorizationPage = (AnchorPane) loader.load();
+            rootLayout.setCenter(authorizationPage);
+            AuthorizationController controller = loader.getController();
+            controller.setMain(this);
         }catch (IOException e){
             e.printStackTrace();
         }
