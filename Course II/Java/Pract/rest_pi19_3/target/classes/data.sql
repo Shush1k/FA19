195e1ddf9237bdffa1654eb083944bdcd59d18f3
Shush1k <Shush1k.b@gmail.com>
2021-03-16 14:25:58 +0300

    rest
    - занятие 15.03

---
+++ Course II/Java/Pract/rest_pi19_3/target/classes/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO person (first_name, last_name, city, street) VALUES
+('Aliko', 'Dangote', 'Moscow', 'Billionaire Industrialist'),
+('Bill', 'Gates', 'Moscow', 'Billionaire Tech Entrepreneur'),
+('Folrunsho', 'Alakija', 'Moscow', 'Billionaire Oil Magnate');
