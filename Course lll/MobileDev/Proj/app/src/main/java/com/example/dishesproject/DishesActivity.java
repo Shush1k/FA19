3aa0131f2af8b9a9885f725aaa4bba607d0ad746
Shush1k <Shush1k.b@gmail.com>
2022-04-02 12:40:24 +0300

    rename&delete useless

---
+++ Course lll/MobileDev/Proj/app/src/main/java/com/example/dishesproject/DishesActivity.java
@@ -0,0 +1,14 @@
+package com.example.dishesproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DishesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dishes);
+    }
+}
